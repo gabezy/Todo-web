294ba83715eeb3987978ebf229f8d35544d10002
gabezy <gabrielmoreira2705@gmail.com>
2025-01-19 23:46:38 -0300

    add registry router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from "./features/login/login.component";
+import {LoginComponent} from "./features/auth/login/login.component";
+import {RegistryComponent} from "./features/auth/registry/registry.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'registry', component: RegistryComponent },
   { path: '', redirectTo: '/login', pathMatch: "full" }
 ];
