b134d29d917ba7d609ad9d86fe01b1db6048db17
gabezy <gabrielmoreira2705@gmail.com>
2025-01-22 11:00:11 -0300

    add todo-card | change completed status and delete task

---
+++ src/app/features/todo/todo-card/todo-card.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {TaskCompletedDTO, TaskDTO} from "../../../core/dtos/task.dto";
+import {NgClass, NgIf} from "@angular/common";
+import {TaskService} from "../../../core/services/task.service";
+
+@Component({
+  selector: 'app-todo-card',
+  standalone: true,
+  imports: [
+    NgClass, NgIf
+  ],
+  templateUrl: './todo-card.component.html',
+  styleUrl: './todo-card.component.sass'
+})
+export class TodoCardComponent {
+
+  @Input()
+  task!: TaskDTO;
+
+  @Output()
+  completedStatusChanged = new EventEmitter<void>();
+
+  @Output()
+  deletedEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  private readonly taskService = inject(TaskService)
+
+  toggleCompleted() {
+    const completedDTO: TaskCompletedDTO = {
+      completed: !this.task.completed
+    }
+
+    this.taskService.changeCompletedStatus(this.task.id, completedDTO).subscribe({
+      complete: () => this.completedStatusChanged.emit(),
+      error: err => console.log({ err })
+    });
+  }
+
+  deleteTask() {
+    this.taskService.deleteTask(this.task.id).subscribe({
+      complete: () => this.deletedEvent.emit(),
+      error: err => console.log({ err })
+    });
+  }
+
+}
