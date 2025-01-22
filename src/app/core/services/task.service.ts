e54a1db24990eb6877fa7a5e942c80ad2e7735ce
gabezy <gabrielmoreira2705@gmail.com>
2025-01-22 11:00:32 -0300

    add task service

---
+++ src/app/core/services/task.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {CreateTaskDTO, TaskCompletedDTO, TaskDTO} from "../dtos/task.dto";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private readonly baseUrl: string;
+
+  constructor(private readonly http: HttpClient) {
+    this.baseUrl = environment.todoApiUrl + '/tasks';
+  }
+
+  createTask(task: CreateTaskDTO): Observable<void> {
+    return this.http.post<void>(this.baseUrl, task);
+  }
+
+  getAllTask(): Observable<TaskDTO[]> {
+    return this.http.get<TaskDTO[]>(this.baseUrl)
+  }
+
+  changeCompletedStatus(id: number, completedStatus: TaskCompletedDTO): Observable<void> {
+    return this.http.patch<void>(`${this.baseUrl}/${id}`, completedStatus);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+
+}
